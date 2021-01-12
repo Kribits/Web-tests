8b98cba66bd29ff171d1f6c42718f372616f4f0d
Kribits <51164073+Kribits@users.noreply.github.com>
2021-01-12 09:29:27 +0200

    Update main.js
    nomainīts uz aktuālo

--- main.js
+++ main.js
@@ -1,2 +1,20 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+let myImage = document.querySelector('img');
+
+myImage.onclick = function () {
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/swedbank-2x.png') {
+        myImage.setAttribute('src', 'images/citadele_logo_ilgas_pantone.png');
+    } else {
+        myImage.setAttribute('src', 'images/swedbank-2x.png');
+    }
+}
+
+let changeBank = document.querySelector('bank');
+
+changeBank.onclick = function () {
+    let myBank = changeBank.getAttribute('src');
+    if (myBank === 'images/citadele_logo_ilgas_pantone.png') {
+        changeBank.setAttribute('bank', 'alt');
+    }
+}
+
